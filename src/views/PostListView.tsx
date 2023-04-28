05671388330b8698911c3f0fd7f1c82450850e19
Eloi <e.baudelle@gmail.com>
2023-04-28 13:30:26 +0200

    Update PostListView.tsx

--- src/views/PostListView.tsx
+++ src/views/PostListView.tsx
@@ -148,7 +148,7 @@ export function PostList(): JSX.Element {
 						</div>
 					</div>
 				)}
-				<div className="w-full flex h-44 justify-center items-center mobile-xl:pb-0 py-20">
+				<div className="w-full flex h-44 justify-center items-center pb-40 pt-20 phone-xl:pb-20  phone-xl:pt-20">
 					<img className="h-12 opacity-40" src={iconTapio} alt="icon-tapio-stories" />
 				</div>
 			</div>
